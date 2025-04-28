ee1800ef7dbc4f06c821721f23cba064af171815
chirtopcatalin <chircatalinionut2@gmail.com>
2025-04-28 18:37:35 +0300

    search collections by citizen id

--- garbage-collection/garbage-collection/Controllers/AdminController.cs
+++ garbage-collection/garbage-collection/Controllers/AdminController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using garbage_collection.Data;      // adjust to your actual Data namespace
+using Microsoft.EntityFrameworkCore;
+using garbage_collection.Data;
 using garbage_collection.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace garbage_collection.Controllers
 {
@@ -44,7 +49,6 @@ namespace garbage_collection.Controllers
             var list = _context.Citizens
                                .OrderBy(c => c.Id)
                                .ToList();
-
             return View(list);
         }
 
@@ -73,11 +77,41 @@ namespace garbage_collection.Controllers
                     ModelState.AddModelError(string.Empty, "Error saving bin: " + ex.Message);
                 }
             }
-
             return View(bin);
         }
 
+        // GET: Admin/CitizenCollections - Form to enter citizen ID
+        [HttpGet]
+        public IActionResult CitizenCollections()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> CitizenCollections(int citizenId)
+        {
+            var citizen = await _context.Citizens.FindAsync(citizenId);
+            if (citizen == null)
+            {
+                ModelState.AddModelError("citizenId", "Citizen not found");
+                return View("Index");
+            }
 
+            var citizenBins = await _context.Bins
+                .Where(b => b.CitizenId == citizenId)
+                .ToListAsync();
+
+            var binIds = citizenBins.Select(b => b.Id).ToList();
+            var collections = await _context.Collections
+                .Where(c => binIds.Contains(c.Bin_id))
+                .OrderByDescending(c => c.Collection_time)
+                .ToListAsync();
+
+            ViewBag.Citizen = citizen;
+            ViewBag.Bins = citizenBins;
+            ViewBag.Collections = collections;
+
+            return View("CitizenCollectionsResult");
+        }
     }
 }
